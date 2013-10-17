0958a77e6bdbc6c582bb7d88144940a88259413b
joselpm21@gmail.com <joselpm21@gmail.com@a03e0626-4ada-1483-78b3-93e40ff01bfc>
2013-10-17 19:23:55 +0000

    Se agrego vista donde se debe logear en babel.
    **Ventana Adquisicion de datos
    *arreglar mensajes y tiempo del log
    *Cambiar animacion del boton observar
    
    **Ventana Modia
    *cuentas fondo del cielo.
    *Reiniciar la imagen al completar la observacion, Mejorar.
    
    **Interfaz Principal (Clase Simulador)
    *Mejorar la interfaz principal.
    *Conectar Botones para cada tipo de prueba (Cambiar la leyenda).
    *Crear y conectar la base de datos.
    *Completar ventana del panel administrativo
    
    **Terminal
    *Agregar comando faltantes
    *Arreglar conxion con qnxs (verificar)
    
    +Verificar el numero de linea con la imagen que va en curso
    (ProcesoFits)
    +Verificar salto de una imagen a otra.
    +Programa explota al cerrar Modia (Desabilitar boton modia al iniciar
    modia).

--- trunk/src/mysubwindow.cpp
+++ trunk/src/mysubwindow.cpp
@@ -4,6 +4,9 @@ MySubWindow::MySubWindow(QWidget *parent) : QMdiSubWindow(parent)
 {
     /* Aquí se coloca todo el código necesario para la creación e
      inicialización del widget */
+    //this->setWindowFlags( Qt::Window | Qt::WindowStaysOnTopHint | Qt::CustomizeWindowHint);
+    this->setWindowFlags( Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
+
 }
 
 void MySubWindow::closeEvent(QCloseEvent *)
